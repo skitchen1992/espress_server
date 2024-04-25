8169f233ac80e9c6fa52d6a3504adbf3f31c8339
nikita <nikita.lavrentyev@mysky.com>
2024-04-25 20:44:39 +0400

    env

--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,7 @@ import {config} from 'dotenv'
 config() // добавление переменных из файла .env в process.env
 
 export const SETTINGS = {
-    PORT: process.env.PORT || 3003,
+    PORT: process.env.PORT,
     PATH: {
         VIDEOS: '/hometask_01/api/videos',
     },
